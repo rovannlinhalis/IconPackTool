2b0c554d1032e76bd1938d6d620d2855df7d0ff5
Rovann Linhalis <rovannlinhalis@hotmail.com>
2021-02-04 17:25:43 -0300

    Update Program.cs

--- IconPackTool/Program.cs
+++ IconPackTool/Program.cs
@@ -13,11 +13,12 @@ namespace IconPackTool
         static void Main(string[] args)
         {
             string dir = AppDomain.CurrentDomain.BaseDirectory;
-            string[] files =  Directory.GetFiles(dir, "*.png", SearchOption.TopDirectoryOnly);
+            string[] files = Directory.GetFiles(dir, "*.png", SearchOption.TopDirectoryOnly);
 
-            List<int> sizes = new List<int>() { 16,24,32,48,64,96,128,160 };
+            List<int> sizes = new List<int>() { 16, 24, 32, 48, 64, 96, 128, 160 };
 
-            Parallel.ForEach(files, file => {
+            Parallel.ForEach(files, file =>
+            {
 
                 if (file.Contains("(") && file.Contains(")."))
                 {
@@ -32,53 +33,57 @@ namespace IconPackTool
 
                     Image state2 = img.AdjustImage(0.7f, 1.2f, 0.8f);
                     Image state3 = img.AdjustImage(1.0f, 1.0f, 0.4f);
-                    Image state4 = img.AdjustImage(1.0f, 1.5f, 1.0f);
+                    Image state4 = img.AdjustImage(1.2f, 0.9f, 1.3f);
                     Image state5 = img.AdjustImage(1.2f, 1.0f, 1.1f);
 
-                    foreach (int s in sizes)
-                    {
-                        string folder = Path.Combine(dir,"iconpack", "simples", s.ToString());
-                        string folderStates = Path.Combine(dir, "iconpack", "states", s.ToString());
-                        if (!Directory.Exists(folder))
-                            Directory.CreateDirectory(folder);
+                    //Parallel.ForEach(sizes, s =>
+                    //{
 
-                        if (!Directory.Exists(folderStates))
-                            Directory.CreateDirectory(folderStates);
-
-                        Image img1 = RedimensionarImagem(img, s, s, PixelFormat.Format32bppArgb);
-                        Image img2 = RedimensionarImagem(state2, s, s, PixelFormat.Format32bppArgb);
-                        Image img3 = RedimensionarImagem(state3, s, s, PixelFormat.Format32bppArgb);
-                        Image img4 = RedimensionarImagem(state4, s, s, PixelFormat.Format32bppArgb);
-                        Image img5 = RedimensionarImagem(state5, s, s, PixelFormat.Format32bppArgb);
-
-                        Bitmap bmpStates = new Bitmap(img.Width * 5, img1.Height, PixelFormat.Format32bppArgb);
-
-                        using (Graphics g = Graphics.FromImage(bmpStates))
+                        foreach (int s in sizes)
                         {
-                            int x = 0;
+                            string folder = Path.Combine(dir, "iconpack", "simples", s.ToString());
+                            string folderStates = Path.Combine(dir, "iconpack", "states", s.ToString());
+                            if (!Directory.Exists(folder))
+                                Directory.CreateDirectory(folder);
 
-                            g.DrawImage(img1, new Point(x, 0));
-                            x += img.Width;
+                            if (!Directory.Exists(folderStates))
+                                Directory.CreateDirectory(folderStates);
 
-                            g.DrawImage(img2, new Point(x, 0));
-                            x += img.Width;
+                            Image img1 = RedimensionarImagem(img, s, s, PixelFormat.Format32bppArgb);
+                            Image img2 = RedimensionarImagem(state2, s, s, PixelFormat.Format32bppArgb);
+                            Image img3 = RedimensionarImagem(state3, s, s, PixelFormat.Format32bppArgb);
+                            Image img4 = RedimensionarImagem(state4, s, s, PixelFormat.Format32bppArgb);
+                            Image img5 = RedimensionarImagem(state5, s, s, PixelFormat.Format32bppArgb);
 
-                            g.DrawImage(img3, new Point(x, 0));
-                            x += img.Width;
+                            Bitmap bmpStates = new Bitmap(img1.Width * 5, img1.Height, PixelFormat.Format32bppArgb);
 
-                            g.DrawImage(img4, new Point(x, 0));
-                            x += img.Width;
+                            using (Graphics g = Graphics.FromImage(bmpStates))
+                            {
+                                int x = 0;
 
-                            g.DrawImage(img5, new Point(x, 0));
-                            x += img.Width;
+                                g.DrawImage(img1, new Point(x, 0));
+                                x += img1.Width;
 
+                                g.DrawImage(img2, new Point(x, 0));
+                                x += img1.Width;
+
+                                g.DrawImage(img3, new Point(x, 0));
+                                x += img1.Width;
+
+                                g.DrawImage(img4, new Point(x, 0));
+                                x += img1.Width;
+
+                                g.DrawImage(img5, new Point(x, 0));
+                                x += img1.Width;
+
+                            }
+
+                            string newPath = Path.Combine(folder, fi.Name.Replace(fi.Extension, "") + "_" + s.ToString() + fi.Extension);
+                            string newPathState = Path.Combine(folderStates, fi.Name.Replace(fi.Extension, "") + "_" + s.ToString() + fi.Extension);
+                            img1.Save(newPath);
+                            bmpStates.Save(newPathState);
                         }
-
-                        string newPath = Path.Combine(folder, fi.Name.Replace(fi.Extension, "") + "_" + s.ToString() + fi.Extension);
-                        string newPathState = Path.Combine(folderStates, fi.Name.Replace(fi.Extension, "") + "_" + s.ToString() + fi.Extension);
-                        img1.Save(newPath);
-                        bmpStates.Save(newPathState);
-                    }
+                    //});
                 }
             });
         }
